4b13496b100e38d402724dbcbeabfcf81d1aea51
divyeshpanchasarawork-creator <divyeshpanchasara.work@gmail.com>
2026-04-18 16:19:25 +0530

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int ans = 0;
+
+        for (int i = 31; i >= 0; i--) {
+            ans <<= 1;
+            ans |= n & 1;
+            n >>= 1;
+        }
+
+        return ans;
+    }
+}
