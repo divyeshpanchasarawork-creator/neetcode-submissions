08ed9d4cd1ebd0554b05ea0c34d5546d4e5db81b
divyeshpanchasarawork-creator <divyeshpanchasara.work@gmail.com>
2026-04-23 14:38:56 +0530

    Add: sum-root-to-leaf-numbers - submission-1

---
+++ Data Structures & Algorithms/sum-root-to-leaf-numbers/submission-1.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        int[] total = {0};
+        helper(root, 0, total);
+        return total[0];
+    }
+
+    private void helper(TreeNode root, int curr, int[] t) {
+        if (root == null) {
+            return;
+        }
+        curr *= 10;
+        curr += root.val;
+        if (root.left == null && root.right == null) {
+            t[0] += curr;
+            return;
+        }
+        helper(root.left, curr, t);
+        helper(root.right, curr, t);
+    }
+}
