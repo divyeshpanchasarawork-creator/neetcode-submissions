1a7a018743c26ab242411eb455f540e8ccfa61fb
divyeshpanchasarawork-creator <divyeshpanchasara.work@gmail.com>
2026-04-30 01:10:49 +0530

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int tribonacci(int n) {
+        int[] memo = new int[n];
+        Arrays.fill(memo, -1);
+        return helper(n, memo);
+    }
+
+    private int helper(int n, int[] memo) {
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+
+        if (memo[n - 1] != -1) return memo[n - 1];
+
+        memo[n - 1] = helper(n - 1, memo) + helper(n - 2, memo) + helper(n - 3, memo);
+
+        return memo[n - 1];
+    }
+}
