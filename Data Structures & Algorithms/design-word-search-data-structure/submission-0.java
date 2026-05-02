327132519013afc9429fec63d5816458e92b5ff9
divyeshpanchasarawork-creator <divyeshpanchasara.work@gmail.com>
2026-05-02 20:04:10 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.java
@@ -0,0 +1,47 @@
+class WordDictionary {
+    private class TrieNode {
+        private final TrieNode[] children = new TrieNode[26];
+        private boolean isEnd;
+    }
+
+    private final TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for (char ch : word.toCharArray()) {
+            if (curr.children[ch - 'a'] == null)
+                curr.children[ch - 'a'] = new TrieNode();
+            curr = curr.children[ch - 'a'];
+        }
+        curr.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        char[] arr = word.toCharArray();
+
+        return helper(0, arr, root);
+    }
+
+    private boolean helper(int i, char[] arr, TrieNode curr) {
+        if (i >= arr.length)
+            return curr.isEnd;
+
+        if (arr[i] == '.') {
+            for (int j = 0; j < 26; j++) {
+                if (curr.children[j] != null && helper(i + 1, arr, curr.children[j])) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        if (curr.children[arr[i] - 'a'] == null)
+            return false;
+
+        return helper(i + 1, arr, curr.children[arr[i] - 'a']);
+    }
+}
