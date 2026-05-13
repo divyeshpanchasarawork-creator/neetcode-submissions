065bc5a084266bd12e393de789109e81095496f8
divyeshpanchasarawork-creator <divyeshpanchasara.work@gmail.com>
2026-05-13 20:49:50 +0530

    Add: final-array-state-after-k-multiplication-operations-i - submission-1

---
+++ Data Structures & Algorithms/final-array-state-after-k-multiplication-operations-i/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            if (a[0] == b[0]) return a[1] - b[1];
+            return a[0] - b[0];
+    });
+        
+        for (int i = 0; i < nums.length; i++) pq.offer(new int[]{nums[i], i});
+
+        while (k-- > 0) {
+            int[] arr = pq.poll();
+            arr[0] *= multiplier;
+            pq.offer(arr);
+        }
+
+        while (!pq.isEmpty()) {
+            int[] arr = pq.poll();
+            nums[arr[1]] = arr[0];
+        }
+
+        return nums;
+    }
+}
