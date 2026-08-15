9c88fcb1d54f3ce4f3190bbab0ebd020f71cfc78
Divyesh Panchasara <divyeshpanchasara.work@gmail.com>
2026-08-15 16:32:47 +0530

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numDecodings(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+
+        if (arr[0] == '0')
+            return 0;
+
+        int next1 = 1;
+        int next2 = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int curr = 0;
+
+            if (arr[i] != '0') {
+                curr = next1;
+
+                if (i + 1 < n) {
+                    int twoDig = (arr[i] - '0') * 10 + (arr[i + 1] - '0');
+
+                    if (twoDig <= 26)
+                        curr += next2;
+                }
+            }
+
+            next2 = next1;
+            next1 = curr;
+        }
+
+        return next1;
+    }
+}
